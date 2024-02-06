0da759779931163f693a31ac068064403b227bdf
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-02-06 09:54:51 +0700

    WIP raster format placeholder

--- rwloader/include/rwloader/raster.hpp
+++ rwloader/include/rwloader/raster.hpp
@@ -15,5 +15,29 @@ namespace RWLoader
         int v_addressing;
         std::string name;
         std::string mask_name;
+
+        int raster_format;
+        union
+        {
+            int d3d_format;
+            int has_alpha;
+        };
+        int width;
+        int height;
+        int depth;
+        int num_mip_levels;
+        int raster_type;
+        union
+        {
+            int compression;
+            struct
+            {
+                bool has_alpha : 1;
+                bool is_cube : 1;
+                bool auto_mip_maps : 1;
+                bool compressed : 1;
+                int pad : 4;
+            };
+        };
     };
 }
